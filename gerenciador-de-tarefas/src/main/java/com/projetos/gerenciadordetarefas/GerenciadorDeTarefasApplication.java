85b1ba736fbb1f54e08fbb4378be720c343e2b9a
norwalneto <norwalgomess@gmail.com>
2024-10-29 11:19:04 -0300

    reiniciando novo projeto.

--- gerenciador-de-tarefas/src/main/java/com/projetos/gerenciadordetarefas/GerenciadorDeTarefasApplication.java
+++ gerenciador-de-tarefas/src/main/java/com/projetos/gerenciadordetarefas/GerenciadorDeTarefasApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerenciadorDeTarefasApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
+	}
 
 }
